dbca92b853df2bca9427a559bbe0c80c81522ec0
DN <hereismyemailaddress@gmx.com>
2014-10-29 14:49:23 -0400

    accelerometer app for key snoop

---
+++ src/com/example/tapsnoop2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.tapsnoop2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.app.Activity;
+import android.*;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+	Sensor accelerometer;
+	SensorManager sm;
+	TextView acceleration;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sm=(SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        
+        acceleration=(TextView)findViewById(R.id.acceleration);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSensorChanged(SensorEvent arg0) {
+		// TODO Auto-generated method stub
+		acceleration.setText("x: "+ arg0.values[0]+
+				"\ny: "+ arg0.values[1]+
+				"\nz: "+ arg0.values[2]);
+	}
+}
